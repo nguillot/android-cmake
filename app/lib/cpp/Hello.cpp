6b939d2101496140e4fd445af16a926bc2bdae6e
Nicolas Guillot <nicolas.guillot@myscript.com>
2016-10-13 17:26:13 +0200

    Try to use the c++_shared STL with clang
    Ending with a runtime crash: couldn't find "libc++_shared.so

--- app/lib/cpp/Hello.cpp
+++ app/lib/cpp/Hello.cpp
@@ -1,7 +1,11 @@
 #include "Hello.h"
+#include <vector>
 
 namespace app {
   std::string Hello::sayHello() const {
-    return "Hello from C++";
+    std::vector<std::string> strs = {"Hello", "from", "C++"};
+    std::string result;
+    for(auto const& s: strs) { result += s+ " "; };
+    return result;
   }
 }
